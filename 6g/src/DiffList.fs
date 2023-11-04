234568b3c7865317b3f86a00db906757fad6959b
Feros47 <Niklasm47@live.dk>
2023-11-04 14:21:33 +0100

    Update DiffList.fs

--- 6g/src/DiffList.fs
+++ 6g/src/DiffList.fs
@@ -26,25 +26,3 @@ let rec inorderD (tree: 'a BTree) : 'a dlist =
 let inorder (tree: 'a BTree) : 'a list =
     let dlist = inorderD tree
     dlist []
-
-// Test the DiffList module
-(*
-let testDiffList () = 
-    // Tree
-    let emptyTree = Leaf
-    let t1 = Branch (Leaf, 1, Leaf)
-    let t2 = Branch (t1, 2, t1)
-    let t3 = Branch (Leaf, 3, Leaf)
-    let t4 = Branch (t3, 4, t3)
-    let t5 = Branch (t2, 5, t4)
-
-    let result = inorder t5
-    printfn "%A" result
-    
-    if result = [1; 2; 1; 5; 3; 4; 3] then
-        printfn "DiffList test Passed"
-    else 
-        printfn "DiffList test failed"
-           
-testDiffList()
-*)
